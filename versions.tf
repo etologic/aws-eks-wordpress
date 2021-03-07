600e50eb50ffcd3c711d64a01dc765cb500f32d7
ETOLOGIC <system.administrator@etologic.com>
2021-03-07 02:34:49 -0600

    initial commit 3

--- versions.tf
+++ versions.tf
@@ -9,18 +9,15 @@ terraform {
     }
     local = {
       source = "hashicorp/local"
-      version = "~> 2.1"
     }
     null = {
       source = "hashicorp/null"
     }
     random = {
       source = "hashicorp/random"
-      version = "~> 2.1"
     }
     template = {
       source = "hashicorp/template"
-      version = "~> 2.1"
     }
   }
 }
